a1fa521d943a18396bb1c6f89744a6c7b824d913
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2024-03-13 05:39:46 -0300

    feat: jump buffer

--- player.gd
+++ player.gd
@@ -1,34 +1,32 @@
 extends CharacterBody2D
 
-
-const SPEED = 150.0
-const JUMP_VELOCITY = -400.0
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var anim = "lick"
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var jump_buffer_time = 0.1
+@export var move_speed = 150.0
+@export var base_velocity = -300.0
+
+var anim = "idle"
 @onready var sprite = $AnimatedSprite2D
 @onready var idle_timer = $IdleTimer
 @onready var lick_timer = $LickTimer
 
-func _ready():
-	sprite.connect("animation_changed", _sprite_anim_changed)
-	idle_timer.connect("timeout", _idle_timeout)
-	lick_timer.connect("timeout", _lick_timeout)
+var _jump_buffer_timer: float = 0
+#func _process(_delta):
+	#if idle_timer.is_stopped() and anim == "idle":
+		#lick_timer.start()
+	#if lick_timer.is_stopped() and anim == "lick":
+		#idle_timer.start()
 
-func _sprite_anim_changed():
-	print(sprite.animation)
 
-func _idle_timeout():
-	print("idle timeout. anim: ", sprite.animation)
-func _lick_timeout():
-	print("lick timeout. anim: ", sprite.animation)
-	
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		_jump_buffer_timer = jump_buffer_time
+
+
 func _process(delta):
-	if idle_timer.is_stopped() and anim == "idle":
-		lick_timer.start()
-	if lick_timer.is_stopped() and anim == "lick":
-		idle_timer.start()
+	_jump_buffer_timer -= delta
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -36,21 +34,22 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept") or _jump_buffer_timer > 0:
+			velocity.y = base_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		anim = "run"
-		velocity.x = direction * SPEED
+		velocity.x = direction * move_speed
 		if direction == -1:
 			sprite.flip_h = true
 		else:
 			sprite.flip_h = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
 
 	if velocity.y < 0:
 		anim = "jump"
@@ -58,10 +57,7 @@ func _physics_process(delta):
 		anim = "fall"
 #
 	if velocity.x == 0 and velocity.y == 0:
-		if idle_timer.time_left > 0:
-			anim = "idle"
-		elif lick_timer.time_left > 0:
-			anim = "lick"
+		anim = "idle"
 
 	sprite.play(anim)
 	move_and_slide()
