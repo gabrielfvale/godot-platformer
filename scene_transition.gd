3b6a3335ce92ab0b4a937a6a650c4ff490bf95d8
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2024-03-16 03:34:42 -0300

    feat: improve transition script

--- scene_transition.gd
+++ scene_transition.gd
@@ -1,4 +1,4 @@
-extends Node
+extends CanvasLayer
 
 
 signal _viewport_set
@@ -11,10 +11,11 @@ var next_path = null
 @export var invert_shader = true
 @export var diamond_pixel_size = 3.5
 
-@onready var texture_rect = $CanvasLayer/TextureRect
+@onready var texture_rect = $TextureRect
+
 
 func _ready():
-	texture_rect.visible = false
+	texture_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
 	print("Current scene: ", current_scene)
@@ -22,14 +23,17 @@ func _ready():
 	_scene_loaded.connect(_animate_transition)
 
 
+func goto_scene(path):
+	next_path = path
+	_set_viewport()
+
+
 func _set_viewport():
 	#print("Setting viewport")
 	await RenderingServer.frame_post_draw
 	var texture = get_viewport().get_texture()
-	#var imgtex = ImageTexture.create_from_image(get_viewport().get_texture().get_image())
 	texture_rect.texture = ImageTexture.create_from_image(texture.get_image())
-	texture_rect.visible = true
-	print("Viewport set")
+	#print("Viewport set")
 	_viewport_set.emit()
 
 
@@ -44,25 +48,6 @@ func _on_viewport_set():
 	current_scene.queue_free()
 	current_scene = new_scene
 	_scene_loaded.emit()
-
-
-func goto_scene(path):
-	next_path = path
-	_set_viewport()
-
-#func _deferred_goto_scene(path):
-	#print("Loading new scene")
-	#current_scene.free()
-	#
-	#var s = ResourceLoader.load(path)
-	#
-	#current_scene = s.instantiate()
-	#
-	#print("New scene loaded: ", current_scene)
-	#_scene_loaded.emit()
-	#
-	#get_tree().root.add_child(current_scene)
-#
 
 
 func _animate_transition():
@@ -80,5 +65,4 @@ func _animate_transition():
 	).from(1.0).set_trans(tween.TRANS_SINE)
 
 	await tween.finished
-	texture_rect.visible = false
 	#print("Transition finished")
